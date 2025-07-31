5d910a421a65fdb15c8658acfc2e08ffec43e7b9
ejasalth <143421718+ejasalth@users.noreply.github.com>
2025-07-31 16:30:18 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="glycobertbart",
+    version="0.1.0",
+    author="Ejas",
+    author_email="ejasalth@buffalo.edu",
+    description="BERT and BART models for glycan structure prediction from LC-MSMS data",
+    url="https://github.com/ejasalth/glycobertbart",
+    packages=find_packages(),
+
+    install_requires=[
+
+    # Glycan analysis
+    "CandyCrunch[draw]==0.6.0",
+    "glycowork~=1.6.1",
+      
+    # Core ML/AI
+    "torch~=2.1",
+    "transformers~=4.50.0",
+    "huggingface-hub~=0.34.0",
+    
+    # Data processing
+    "pandas",
+    "numpy",
+
+    # Mass spectrometry
+    "pyopenms~=3.4.0",],
+    
+    include_package_data=True,
+    package_data={'glycobertbart': ['*.pkl', '*.json', '*pt']}, 
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+    long_description=open("README.md", encoding="utf-8").read(),                      
+    long_description_content_type="text/markdown"
+  
+)
